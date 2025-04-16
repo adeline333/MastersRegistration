4a78ba9742c331d1cdd2cb50b5d9492729aea601
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-04-16 23:47:00 +0200

    logon form

---
+++ login.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace MastersRegistration
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.passwordLogin = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.usernameLogin = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.Label();
+            this.helpLogin = new System.Windows.Forms.LinkLabel();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(439, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.passwordLogin);
+            this.panel1.Controls.Add(this.password);
+            this.panel1.Controls.Add(this.usernameLogin);
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Location = new System.Drawing.Point(245, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 343);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // passwordLogin
+            // 
+            this.passwordLogin.Location = new System.Drawing.Point(157, 125);
+            this.passwordLogin.Name = "passwordLogin";
+            this.passwordLogin.PasswordChar = '*';
+            this.passwordLogin.Size = new System.Drawing.Size(100, 22);
+            this.passwordLogin.TabIndex = 8;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(155, 97);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(67, 16);
+            this.password.TabIndex = 7;
+            this.password.Text = "Password";
+            // 
+            // usernameLogin
+            // 
+            this.usernameLogin.Location = new System.Drawing.Point(158, 62);
+            this.usernameLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.usernameLogin.Name = "usernameLogin";
+            this.usernameLogin.Size = new System.Drawing.Size(163, 22);
+            this.usernameLogin.TabIndex = 6;
+            this.usernameLogin.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(154, 30);
+            this.username.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(70, 16);
+            this.username.TabIndex = 5;
+            this.username.Text = "Username";
+            this.username.Click += new System.EventHandler(this.username_Click);
+            // 
+            // helpLogin
+            // 
+            this.helpLogin.AutoSize = true;
+            this.helpLogin.Location = new System.Drawing.Point(458, 297);
+            this.helpLogin.Name = "helpLogin";
+            this.helpLogin.Size = new System.Drawing.Size(175, 16);
+            this.helpLogin.TabIndex = 9;
+            this.helpLogin.TabStop = true;
+            this.helpLogin.Text = "Need Help?/ create account";
+            this.helpLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.helpLogin_LinkClicked);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonLogin.Location = new System.Drawing.Point(319, 290);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 8;
+            this.buttonLogin.Text = "LOG IN";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.helpLogin);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login";
+            this.Load += new System.EventHandler(this.login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox passwordLogin;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox usernameLogin;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.LinkLabel helpLogin;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
---
+++ login.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MastersRegistration
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-SGKPF3E\\SQLEXPRESS;Initial Catalog=REGISTRATION;Integrated Security=True;TrustServerCertificate=True");
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+           
+
+                try
+                {
+                    conn.Open();
+
+
+                    string query = "SELECT studentName, password FROM STUDENT WHERE studentName = @StudentName AND password =HASHBYTES('SHA2_256', @passwords)";
+
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@StudentName", usernameLogin.Text.Trim());
+                    command.Parameters.AddWithValue("@Passwords", passwordLogin.Text.Trim());
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            MessageBox.Show("Login successful!");
+                            Home home1 = new Home();
+                            home1.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid username or password", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+            }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            finally // ✅ Ensure connection is always closed
+            {
+                if (conn.State == ConnectionState.Open) // ✅ Check if still open
+                {
+                    conn.Close(); // ✅ Close the connection
+                }
+            }
+
+        }
+
+        private void helpLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            sign_up signUpForm= new sign_up();
+            signUpForm.Show();
+
+            //this.Hide();
+
+            //login loginForm=new login();
+            //loginForm.Hide();
+        }
+
+        private void username_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
