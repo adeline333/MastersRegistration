7250638c075d77489989f213400b46d5494a94a1
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-04-21 23:51:20 +0200

    schedule form

---
+++ schedule.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace MastersRegistration
+{
+    partial class schedule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.courseCode = new System.Windows.Forms.Label();
+            this.assignedLecturer = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.startDate = new System.Windows.Forms.Label();
+            this.endDate = new System.Windows.Forms.Label();
+            this.remove = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.search = new System.Windows.Forms.Button();
+            this.searchTB = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.view = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(286, 286);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 167);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(407, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Faculty details";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SCHEDULE";
+            // 
+            // courseCode
+            // 
+            this.courseCode.AutoSize = true;
+            this.courseCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.courseCode.Location = new System.Drawing.Point(1, 41);
+            this.courseCode.Name = "courseCode";
+            this.courseCode.Size = new System.Drawing.Size(94, 16);
+            this.courseCode.TabIndex = 14;
+            this.courseCode.Text = "Couse code:";
+            // 
+            // assignedLecturer
+            // 
+            this.assignedLecturer.AutoSize = true;
+            this.assignedLecturer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.assignedLecturer.Location = new System.Drawing.Point(1, 87);
+            this.assignedLecturer.Name = "assignedLecturer";
+            this.assignedLecturer.Size = new System.Drawing.Size(136, 16);
+            this.assignedLecturer.TabIndex = 15;
+            this.assignedLecturer.Text = "Assigned Lecturer:";
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.add.Font = new System.Drawing.Font("Lucida Bright", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.add.Location = new System.Drawing.Point(4, 152);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(133, 38);
+            this.add.TabIndex = 19;
+            this.add.Text = "Add Schedule";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // update
+            // 
+            this.update.BackColor = System.Drawing.Color.ForestGreen;
+            this.update.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.update.Location = new System.Drawing.Point(168, 155);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(138, 38);
+            this.update.TabIndex = 21;
+            this.update.Text = "Update schedule";
+            this.update.UseVisualStyleBackColor = false;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // startDate
+            // 
+            this.startDate.AutoSize = true;
+            this.startDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startDate.Location = new System.Drawing.Point(367, 45);
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(80, 16);
+            this.startDate.TabIndex = 23;
+            this.startDate.Text = "Start Date:";
+            // 
+            // endDate
+            // 
+            this.endDate.AutoSize = true;
+            this.endDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.endDate.Location = new System.Drawing.Point(367, 87);
+            this.endDate.Name = "endDate";
+            this.endDate.Size = new System.Drawing.Size(75, 16);
+            this.endDate.TabIndex = 24;
+            this.endDate.Text = "End Date:";
+            // 
+            // remove
+            // 
+            this.remove.BackColor = System.Drawing.Color.Red;
+            this.remove.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.remove.ForeColor = System.Drawing.SystemColors.Info;
+            this.remove.Location = new System.Drawing.Point(381, 152);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(166, 38);
+            this.remove.TabIndex = 27;
+            this.remove.Text = "Remove schedule";
+            this.remove.UseVisualStyleBackColor = false;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.linkLabel5);
+            this.panel1.Controls.Add(this.linkLabel4);
+            this.panel1.Controls.Add(this.update);
+            this.panel1.Controls.Add(this.linkLabel3);
+            this.panel1.Controls.Add(this.linkLabel2);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.dateTimePicker2);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.remove);
+            this.panel1.Controls.Add(this.endDate);
+            this.panel1.Controls.Add(this.startDate);
+            this.panel1.Controls.Add(this.add);
+            this.panel1.Controls.Add(this.assignedLecturer);
+            this.panel1.Controls.Add(this.courseCode);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(63, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(666, 236);
+            this.panel1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 38;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 37;
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Location = new System.Drawing.Point(566, 235);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(43, 13);
+            this.linkLabel5.TabIndex = 36;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "teacher";
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Location = new System.Drawing.Point(566, 207);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(52, 13);
+            this.linkLabel4.TabIndex = 35;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "Schedule";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(566, 180);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(58, 13);
+            this.linkLabel3.TabIndex = 34;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "registration";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(577, 149);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(41, 13);
+            this.linkLabel2.TabIndex = 33;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Faculty";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(577, 121);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel1.TabIndex = 32;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Course";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(454, 86);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 31;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(454, 45);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 30;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // search
+            // 
+            this.search.BackColor = System.Drawing.Color.ForestGreen;
+            this.search.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.search.Location = new System.Drawing.Point(12, 263);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(138, 38);
+            this.search.TabIndex = 22;
+            this.search.Text = "Search";
+            this.search.UseVisualStyleBackColor = false;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // searchTB
+            // 
+            this.searchTB.Location = new System.Drawing.Point(168, 273);
+            this.searchTB.Name = "searchTB";
+            this.searchTB.Size = new System.Drawing.Size(100, 20);
+            this.searchTB.TabIndex = 38;
+            this.searchTB.TextChanged += new System.EventHandler(this.searchTB_TextChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 307);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(208, 143);
+            this.dataGridView2.TabIndex = 39;
+            // 
+            // view
+            // 
+            this.view.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.view.Font = new System.Drawing.Font("Lucida Bright", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.view.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.view.Location = new System.Drawing.Point(554, 256);
+            this.view.Name = "view";
+            this.view.Size = new System.Drawing.Size(98, 27);
+            this.view.TabIndex = 40;
+            this.view.Text = "view";
+            this.view.UseVisualStyleBackColor = false;
+            this.view.Click += new System.EventHandler(this.view_Click);
+            // 
+            // schedule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.view);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.searchTB);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Name = "schedule";
+            this.Text = "schedule";
+            this.Load += new System.EventHandler(this.schedule_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label courseCode;
+        private System.Windows.Forms.Label assignedLecturer;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Label startDate;
+        private System.Windows.Forms.Label endDate;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.TextBox searchTB;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button view;
+    }
+}
---
+++ schedule.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MastersRegistration
+{
+    public partial class schedule : Form
+    {
+        public schedule()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-SGKPF3E\\SQLEXPRESS;Initial Catalog=REGISTRATION;Integrated Security=True;TrustServerCertificate=True");
+        private void update_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE SCHEDULE SET " +
+             "lecturer = @lecturer, startDate = @start, endDate = @end WHERE courseId = @cId";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@lecturer", textBox2.Text.Trim());
+            cmd.Parameters.AddWithValue("@start", dateTimePicker1.Text.Trim());
+            cmd.Parameters.AddWithValue("@end", dateTimePicker2.Text.Trim());
+            cmd.Parameters.AddWithValue("@cId", textBox1.Text.Trim());
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            DisplayScheduleDataGridView();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            faculty sourse = new faculty();
+            sourse.Show();
+            this.Hide();
+
+
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            sear sourse = new sear();
+            sourse.Show();
+            this.Hide();
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            course sourse1 = new course();
+            sourse1.Show();
+            this.Hide();
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            schedule one = new schedule();
+            one.Show();
+            this.Hide();
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            teacher teacher1 = new teacher();
+            teacher1.Show();
+            this.Hide();
+        }
+        private void DisplayScheduleDataGridView()
+        {
+            string query = "SELECT * FROM SCHEDULE";
+            conn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+            DataSet ds = new DataSet();
+            sda.Fill(ds, " SCHEDULE");
+            dataGridView1.DataSource = ds.Tables[" SCHEDULE"];
+            conn.Close();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO SCHEDULE (courseId, lecturer, startDate, EndDate) VALUES (@SId, @SName, @SDescription,@SDescription2)";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@SId", textBox1.Text);
+                cmd.Parameters.AddWithValue("@SName", textBox2.Text);
+                cmd.Parameters.AddWithValue("@SDescription", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@SDescription2", dateTimePicker2.Text);
+
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conn.Close();
+                    DisplayScheduleDataGridView();  // Refresh the data grid after the insertion
+                }
+            }
+        }
+
+        private void schedule_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DisplayScheduleDataGridSearch()
+        {
+
+            string query = "SELECT * FROM SCHEDULE WHERE courseId = @cId";
+            SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+            sda.SelectCommand.Parameters.AddWithValue("@cId", searchTB.Text);
+
+            DataSet ds = new DataSet();
+            conn.Open();
+            sda.Fill(ds, "SCHEDULE");
+            conn.Close();
+
+            dataGridView2.DataSource = ds.Tables["SCHEDULE"];
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            string query = "DELETE FROM SCHEDULE WHERE courseId = @cId";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@cId", textBox1.Text.Trim());
+
+            conn.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            if (rowsAffected > 0)
+            {
+                MessageBox.Show("schedule deleted successfully!");
+            }
+            else
+            {
+                MessageBox.Show("No FACULTY found with the givenFACULTY ID.");
+            }
+
+           DisplayScheduleDataGridView();
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            DisplayScheduleDataGridSearch();
+        }
+
+        private void searchTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void view_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO SCHEDULE (courseId, lecturer, startDate, EndDate) VALUES (@SId, @SName, @SDescription,@SDescription2)";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@SId", textBox1.Text);
+                cmd.Parameters.AddWithValue("@SName", textBox2.Text);
+                cmd.Parameters.AddWithValue("@SDescription", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@SDescription2", dateTimePicker2.Text);
+
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conn.Close();
+                    DisplayScheduleDataGridView();  // Refresh the data grid after the insertion
+                }
+            }
+        }
+    }
+    }
+
